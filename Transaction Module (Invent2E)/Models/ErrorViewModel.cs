808eb78a3b1cc5b4dc4f5a5b8be1cb44d89227ff
User <User@DESKTOP-U5AI1AI>
2025-04-29 00:57:23 +0800

    Add project files.

---
+++ Transaction Module (Invent2E)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Transaction_Module__Invent2E_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
