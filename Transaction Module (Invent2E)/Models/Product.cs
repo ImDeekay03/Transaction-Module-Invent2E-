344979308fbdfdb0f4f50651e14732eb061744a1
Angelo Jose Damaso <damaso.23-2345@qcu.edu.ph>
2025-05-01 13:40:45 +0800

    Updated Product.cs

--- Transaction Module (Invent2E)/Models/Product.cs
+++ Transaction Module (Invent2E)/Models/Product.cs
@@ -4,6 +4,21 @@ namespace Transaction_Module__Invent2E_.Models
 {
     public class Product
     {
+        public int ProductId { get; set; } // Unique ID for each product
+        public string ProductName { get; set; } // e.g., "Men Cotton Polo Shirt"
+        public string SKU {  get; set; } // e.g., "ADDS-GRN-22"
+        public string Size { get; set; } // e.g., "L", "XL"
 
+        public int Quantity { get; set; } // e.g., 2 (pcs)
+        public decimal Rate { get; set; } // e.g., 120.00
+        public decimal Taxrate { get; set; } // e.g., 0.50 for 50%
+
+        public decimal Subtotal => Quantity * Rate * (1 + Taxrate); // Auto-calculated subtotal
+        public int OrderId { get; set; } // Foreign key to Order table
+
+        // Metadata (optional, for linking source)
+        public string SourceModule {  get; set; } // e.g., "Supplier" or "Customer"
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
